a1acb74fb8dbd993307d9583b6b2ee440941286c
Ryan <leehahnwoong@gmail.com>
2023-01-22 18:31:33 -0800

    [add] navigation bar mid-commit including all components

--- frontend/src/components/NaviBar/index.tsx
+++ frontend/src/components/NaviBar/index.tsx
@@ -1,36 +1,31 @@
 import * as React from "react";
 import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 import "./style.css";
-import uci_logo from "../../assets/images/uci_logo.png";
+import uci_logo from "../../assets/image/uci_logo.png";
+import logout from "../../assets/image/logout.png";
 
 const Navibar = () => {
   return (
-    <Navbar className="navbar-body" collapseOnSelect expand="lg" variant="dark">
+    <Navbar className="navbar-body">
       <Navbar.Brand href="/">
         <img
           src={uci_logo}
           height="40"
           className="d-inline-block align-top navbar-logo"
-          alt="Techeer Logo"
+          alt="UCI Logo"
         />{" "}
         <span className="navbar-title">UCI Home</span>
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto navbar-link">
           <Nav.Link href="/soc">Schedule of Classes</Nav.Link>
           <Nav.Link href="/studentaccess">Student Access</Nav.Link>
           <Nav.Link href="/webreg">WebReg</Nav.Link>
           <Nav.Link href="/siteindex">Site Index</Nav.Link>
+          <Nav.Link href="/logout">
+            <img src={logout} alt="logout" width={70}/>
+          </Nav.Link>
         </Nav>
-        {/* <Nav>
-          <NavDropdown title="Profile" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Register</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
-          </NavDropdown>
-        </Nav> */}
       </Navbar.Collapse>
     </Navbar>
   );
