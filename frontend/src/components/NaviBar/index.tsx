c19008d63007271512331c06299300a662b6b5a4
Ryan <leehahnwoong@gmail.com>
2023-01-22 17:12:18 -0800

    [add] basic navigation Bar

--- frontend/src/components/NaviBar/index.tsx
+++ frontend/src/components/NaviBar/index.tsx
@@ -1,40 +1,36 @@
 import * as React from "react";
 import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 import "./style.css";
-import Techeer_Logo_Basic from "../../assets/images/Techeer_Logo_Basic.png";
-import Techeer_Logo_Circle from "../../assets/images/Techeer_Logo_circle.png";
+import uci_logo from "../../assets/images/uci_logo.png";
 
 const Navibar = () => {
   return (
     <Navbar className="navbar-body" collapseOnSelect expand="lg" variant="dark">
-      {/* <Navbar.Brand href="/">
-        Techeer;
-      </Navbar.Brand> */}
       <Navbar.Brand href="/">
         <img
-          src={Techeer_Logo_Circle}
+          src={uci_logo}
           height="40"
           className="d-inline-block align-top navbar-logo"
           alt="Techeer Logo"
         />{" "}
-        <span className="navbar-title">Techeer</span>
+        <span className="navbar-title">UCI Home</span>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto navbar-link">
-          <Nav.Link href="/project">Projects</Nav.Link>
-          <Nav.Link href="/member">Members</Nav.Link>
-          {/* 보류 기능 */}
-          {/* <Nav.Link href="/board">Board</Nav.Link> */}
+          <Nav.Link href="/soc">Schedule of Classes</Nav.Link>
+          <Nav.Link href="/studentaccess">Student Access</Nav.Link>
+          <Nav.Link href="/webreg">WebReg</Nav.Link>
+          <Nav.Link href="/siteindex">Site Index</Nav.Link>
         </Nav>
-        <Nav>
+        {/* <Nav>
           <NavDropdown title="Profile" id="collasible-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">Register</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
           </NavDropdown>
-        </Nav>
+        </Nav> */}
       </Navbar.Collapse>
     </Navbar>
   );
