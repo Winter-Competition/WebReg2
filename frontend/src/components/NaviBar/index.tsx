ad2a4fbd9d51cc25c7f99d634a11065b204c4c03
Ryan <leehahnwoong@gmail.com>
2023-01-22 21:55:04 -0800

    [add]bootstrap and use collapsed with scrolling

--- frontend/src/components/NaviBar/index.tsx
+++ frontend/src/components/NaviBar/index.tsx
@@ -1,32 +1,69 @@
 import * as React from "react";
-import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import { Navbar, Nav,Container, Button } from "react-bootstrap";
 import "./style.css";
 import uci_logo from "../../assets/image/uci_logo.png";
 import logout from "../../assets/image/logout.png";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Navibar = () => {
+//   return (
+//     <Navbar className="navbar-body" collapseOnSelect expand="lg" variant="dark">
+//       <Container>
+//       <Navbar.Brand href="/">
+//         <img
+//           src={uci_logo}
+//           height="40"
+//           className="d-inline-block align-top navbar-logo"
+//           alt="UCI Logo"
+//         />
+//         <span className="navbar-title">UCI Home</span>
+//       </Navbar.Brand>
+//       <Navbar.Collapse id="responsive-navbar-nav me-auto">
+//         <Nav className="me-auto navbar-link">
+//           <Nav.Link href="/soc">Schedule of Classes</Nav.Link>
+//           <Nav.Link href="/studentaccess">Student Access</Nav.Link>
+//           <Nav.Link href="/webreg">WebReg</Nav.Link>
+//           <Nav.Link href="/siteindex">Site Index</Nav.Link>
+//           <Container className="navbar-logout">
+//             <Nav.Link href="/logout">
+//               <img src={logout} alt="logout" width={70}/>
+//             </Nav.Link>
+//           </Container>
+//         </Nav>
+//       </Navbar.Collapse>
+//       </Container>
+//     </Navbar>
+//   );
+// };
   return (
-    <Navbar className="navbar-body">
-      <Navbar.Brand href="/">
+    <Navbar expand="lg" className="navbar-body" collapseOnSelect>
+      <Container fluid>
+        <Navbar.Brand href="/">
         <img
           src={uci_logo}
           height="40"
-          className="d-inline-block align-top navbar-logo"
+          className="d-inline-block navbar-logo"
           alt="UCI Logo"
-        />{" "}
-        <span className="navbar-title">UCI Home</span>
-      </Navbar.Brand>
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="me-auto navbar-link">
-          <Nav.Link href="/soc">Schedule of Classes</Nav.Link>
-          <Nav.Link href="/studentaccess">Student Access</Nav.Link>
-          <Nav.Link href="/webreg">WebReg</Nav.Link>
-          <Nav.Link href="/siteindex">Site Index</Nav.Link>
-          <Nav.Link href="/logout">
-            <img src={logout} alt="logout" width={70}/>
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+        />UCI Home</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="/soc">Home</Nav.Link>
+            <Nav.Link href="/studentaccess">Student Access</Nav.Link>
+            <Nav.Link href="/webreg">WebReg</Nav.Link>
+            <Nav.Link href="/index">Site Index</Nav.Link>
+          </Nav>
+           <Nav.Link href="/logout">
+            <img src={logout} alt="logout_png">
+            </img>
+            </Nav.Link>
+            {/* <Button variant="outline-dark btn-sm">Logout</Button> */}
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
