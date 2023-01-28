408623b7fd01cbca5ea03f0be1ef164e89062344
Ryan <leehahnwoong@gmail.com>
2023-01-28 15:02:02 -0800

    [add] testing card components

--- frontend/src/components/Cards/index.tsx
+++ frontend/src/components/Cards/index.tsx
@@ -6,6 +6,14 @@ import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logout from "../../assets/image/logout.png";
 
+const class_code = "35630"
+const class_days = "Tu/Th"
+const time_start = "9:30"
+const time_end = "10:50"
+const am_pm = "AM"
+const building = "EH 1200"
+
+
 const ClassCart = () => {
   const [show, setShow] = useState(false);
 
