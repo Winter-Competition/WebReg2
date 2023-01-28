8bde61cdc39fad9f7d4a3163721a03afae58b382
Ryan <leehahnwoong@gmail.com>
2023-01-28 14:55:45 -0800

    [add] cart offcanvas

--- frontend/src/pages/CheckOut/index.tsx
+++ frontend/src/pages/CheckOut/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import RegisterForm from "../../components/Register";
+import ClassCart from "../../components/Cart";
 
 export default function CheckOut() {
   return (
